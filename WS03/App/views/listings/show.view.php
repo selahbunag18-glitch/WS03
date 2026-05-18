fb5ac83f97fe5b45cba17186043c99dd4fd11959
nethuselah sp bunag <240694359+selahbunag18-glitch@users.noreply.github.com>
2026-05-18 16:26:11 +0800

    Update WS03

--- WS03/App/views/listings/show.view.php
+++ WS03/App/views/listings/show.view.php
@@ -59,8 +59,7 @@
         Put "Job Application" as the subject of your email and attach your
         resume.
     </p>
-    <a
-        href="https://mail.google.com/mail/?view=cm&to=<?= htmlspecialchars($listing->email) ?>"
+    <a href="<?= Framework\Session::has('user') ? 'https://mail.google.com/mail/?view=cm&to=' . htmlspecialchars($listing->email) : '/auth/login' ?>"
         class="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium cursor-pointer text-indigo-700 bg-indigo-100 hover:bg-indigo-200">
         Apply Now
     </a>
